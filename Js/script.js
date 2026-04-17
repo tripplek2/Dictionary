5b19ef25916310f719258863d5b7594119745648
Kelvin Korir <kibetkelvin222@gmail.com>
2026-04-17 13:11:24 +0300

    converting synonyms into html using span

--- Js/script.js
+++ Js/script.js
@@ -41,6 +41,8 @@ async function fetchWord(word) {
         let synonymsHTML = "";
         const synonyms = meaning.definitions[0].synonyms || [];
 
+        synonymsHTML = synonyms.map(s => `<span>${s}</span>`).join("");
+
 
     }
 }
